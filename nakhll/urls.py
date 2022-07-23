19b4e7a8d1a4ef07191cfd8b785df540afe95776
likecodingloveproblems <mohammadali.mehdizadeh@outlook.com>
2022-07-23 13:24:49 +0430

    configure djoser

--- nakhll/urls.py
+++ nakhll/urls.py
@@ -6,11 +6,6 @@ from . import setup as nakhll_setup
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
 
 # from reports.admin import reports_admin_site
 nakhll_setup.seutp()
@@ -52,27 +47,23 @@ api_urls = [
 ]
 
 urlpatterns = [
-    re_path(
-        r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(
-            cache_timeout=0),
-        name='schema-json'),
-    re_path(
-        r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-        name='schema-swagger-ui'),
-    re_path(
-        r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc'),
-    path(
-        'api/token/', TokenObtainPairView.as_view(),
-        name='token_obtain_pair'),
-    path(
-        'api/token/refresh/', TokenRefreshView.as_view(),
-        name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    re_path(r'^auth/', include('djoser.urls')),
     path('', include(view_urls)),
     path('api/v1/', include(api_urls)), ]
 
 if settings.DEBUG:
+    urlpatterns += [
+        re_path(
+            r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(
+                cache_timeout=0),
+            name='schema-json'),
+        re_path(
+            r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
+        re_path(
+            r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'),
+    ]
     urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')))
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
